3b4818628db5f40e425229a606ac0a0afdb61fed
James Fennell <jamespfennell@gmail.com>
2023-08-20 11:55:11 -0400

    save

--- db/schema/007_postgis.sql
+++ db/schema/007_postgis.sql
@@ -1,4 +1,7 @@
 CREATE EXTENSION postgis;
 ALTER TABLE stop ADD COLUMN location geography(POINT);
 UPDATE STOP SET location = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+ALTER TABLE stop ALTER COLUMN location SET NOT NULL;
 
+ALTER TABLE stop DROP COLUMN latitude;
+ALTER TABLE stop DROP COLUMN longitude;
